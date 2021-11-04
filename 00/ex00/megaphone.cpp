04f2a6a7a8b6ccec6d6327ba8dbd80ff8be12b0b
flohrel <flohrel@student.42.fr>
2021-11-04 17:44:09 +0100

    megaphone OK

--- 00/ex00/megaphone.cpp
+++ 00/ex00/megaphone.cpp
@@ -3,16 +3,22 @@
 
 char my_toupper(char ch)
 {
-    return static_cast<char>(std::toupper(static_cast<unsigned char>(ch)));
+    return (static_cast<char>(std::toupper(static_cast<unsigned char>(ch))));
 }
 
-int main(void)
+int main(int argc, char **argv)
 {
-    std::string  str;
-
-    std::cin >> str;
-    for (std::string::iterator it = str.begin(); it != str.end; ++it)
-    {
-        toupper
+    std::string str;
+    if (argc == 1) {
+        std::cout << "* LOUD AND UNBEARABLE NOISE *";
     }
+    else {
+        for (int i = 1; i < argc; ++i) {
+            str = argv[i];
+            for (std::string::iterator it = str.begin(); it != str.end(); ++it) {
+                std::cout << my_toupper(*it);
+            }
+        }
+    }
+    std::cout << std::endl;
 }
