be8bba397b07ca482e2f2ff2b1ab546adf8ad350
flohrel <flohrel@student.42.fr>
2021-12-20 16:46:46 +0100

    07 OK

--- 07/ex02/src/main.cpp
+++ 07/ex02/src/main.cpp
@@ -3,4 +3,37 @@
 
 int	main( void )
 {
+	Array<int>			count(10);
+	Array<int>			count2(10);
+	Array<std::string>	str(1);
+
+	for (int i = 0; i < 10; i++)
+	{
+		count[i] = i + 1;
+		count2[i] = 10 - i;
+	}
+	str[0] = "Hello world";
+	{
+		try
+		{
+			std::cout << str[0] << std::endl;
+			std::cout << str[1] << std::endl;
+		}
+		catch (std::exception& e)
+		{
+			std::cout << e.what() << std::endl;
+		}
+	}
+	{
+		for (int i = 0; i < 10; i++)
+		{
+			std::cout << count[i] << " ";
+		}
+		std::cout << std::endl;
+		count = count2;
+		for (int i = 0; i < 10; i++)
+		{
+			std::cout << count[i] << " ";
+		}
+	}
 }
