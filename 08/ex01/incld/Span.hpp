466f50b6b12027f12fb1b428f610bd596fa818dd
flohrel <flohrel@student.42.fr>
2021-12-22 09:01:38 +0100

    08/ex01 OK

--- 08/ex01/incld/Span.hpp
+++ 08/ex01/incld/Span.hpp
@@ -1,44 +1,45 @@
 #ifndef __SPAN__H__
 #define __SPAN__H__
 
+#include <algorithm>
+#include <exception>
+#include <vector>
+
 class Span
 {
-	public:
-		Span( unsigned int n );
-		Span( Span const & src );
-		~Span( void );
+	private:
+		unsigned int		_maxSize;
+		std::vector<int>	_numbers;
+
+		Span( void );
 
 		Span &	operator=( Span const & rhs );
 		
 	
-	private:
-		unsigned int	_maxSize;
-		unsigned int	_size;
-		Span( void );
+	public:
+		Span( unsigned int n );
+		Span( Span const& src );
+		~Span( void );
+
+		unsigned int		getMaxSize( void ) const;
+		std::vector<int>	getNumbers( void ) const;
+		void				addNumber( int number );
+		unsigned int		shortestSpan( void );
+		unsigned int		longestSpan( void );
+
+		class ContainerFull: public std::exception
+		{
+			public:
+				virtual char const *	what() const throw();
+		};
+
+		class ContainerEmpty: public std::exception
+		{
+			public:
+				virtual char const *	what() const throw();
+		};
+
 
 };
-
-Span::Span( unsigned int n ) : _maxSize(n), _size(0)
-{
-	return;
-}
-
-Span::Span( Span const & src )
-{
-	*this = src;
-	return;
-}
-
-Span::~Span( void )
-{
-	/* Content */
-	return;
-}
-
-Span &	Span::operator=( Span const & rhs )
-{
-	/* Content */
-	return (*this);
-}
 
 #endif
---
+++ 08/ex01/src/Span.cpp
@@ -0,0 +1,83 @@
+#include "Span.hpp"
+
+Span::Span( unsigned int n ) : _maxSize(n)
+{
+	return;
+}
+
+Span::Span( Span const & src )
+{
+	*this = src;
+	return;
+}
+
+Span::~Span( void )
+{
+	return;
+}
+
+unsigned int		Span::getMaxSize( void ) const
+{
+	return (this->_maxSize);
+}
+
+std::vector<int>	Span::getNumbers( void ) const
+{
+	return (this->_numbers);
+}
+
+Span &	Span::operator=( Span const & rhs )
+{
+	this->_maxSize = rhs.getMaxSize();
+	this->_numbers = rhs.getNumbers();
+	return (*this);
+}
+
+char const*	Span::ContainerEmpty::what() const throw()
+{
+	return ("Not enough number in the container.");
+}
+
+char const*	Span::ContainerFull::what() const throw()
+{
+	return ("Container is already full.");
+}
+
+void	Span::addNumber( int number )
+{
+	if (this->_numbers.size() == this->_maxSize)
+		throw (Span::ContainerFull());
+	this->_numbers.push_back(number);
+	std::sort(this->_numbers.begin(), this->_numbers.end());
+}
+
+unsigned int	Span::shortestSpan( void )
+{
+	int							result;
+	std::vector<int>::iterator	it;
+	std::vector<int>::iterator	it2;
+
+	if (this->_numbers.size() < 2)
+		throw (Span::ContainerEmpty());
+	it = this->_numbers.begin();
+	it2 = it + 1;
+	result = *(this->_numbers.end() - 1) - *it;
+	for (; it2 != this->_numbers.end(); ++it, ++it2)
+	{
+		if (*it2 - *it < result)
+		{
+			result = *it2 - *it;
+		}
+	}
+	return (result);
+}
+
+unsigned int	Span::longestSpan( void )
+{
+	int	result;
+
+	if (this->_numbers.size() < 2)
+		throw (Span::ContainerEmpty());
+	result = *(this->_numbers.end() - 1) - *(this->_numbers.begin());
+	return (result);
+}
