631fa8f9eec55c6defa6c839b995d45fc81ba8eb
flohrel <flohrel@student.42.fr>
2021-12-27 18:12:38 +0100

    08/ex02 OK

--- 08/ex02/incld/MutantStack.hpp
+++ 08/ex02/incld/MutantStack.hpp
@@ -8,7 +8,8 @@ template< class T >
 class MutantStack: public std::stack<T>
 {
 	public:
-		typedef typename std::stack<T>::container_type::iterator iterator;
+		typedef typename std::stack<T>::container_type::iterator	iterator;
+
 		iterator	begin( void ) { return std::stack<T>::c.begin(); };
 		iterator	end( void ) { return std::stack<T>::c.end(); };
 };
