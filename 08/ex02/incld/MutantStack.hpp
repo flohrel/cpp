9ff88a2c8411e2f3108dfdfdb7dc86f52228f0aa
Florian OHREL <flohrel@student.42.fr>
2022-04-20 00:49:29 +0200

    ADD const, rev, const_rev iterators + TESTS

--- 08/ex02/incld/MutantStack.hpp
+++ 08/ex02/incld/MutantStack.hpp
@@ -8,10 +8,19 @@ template< class T >
 class MutantStack: public std::stack<T>
 {
 	public:
-		typedef typename std::stack<T>::container_type::iterator	iterator;
+		typedef typename std::stack<T>::container_type::iterator				iterator;
+		typedef typename std::stack<T>::container_type::const_iterator			const_iterator;
+		typedef typename std::stack<T>::container_type::reverse_iterator		reverse_iterator;
+		typedef typename std::stack<T>::container_type::const_reverse_iterator	const_reverse_iterator;
 
-		iterator	begin( void ) { return std::stack<T>::c.begin(); };
-		iterator	end( void ) { return std::stack<T>::c.end(); };
+		iterator				begin( void ) { return std::stack<T>::c.begin(); };
+		const_iterator			begin( void ) const { return std::stack<T>::c.begin(); };
+		iterator				end( void ) { return std::stack<T>::c.end(); };
+		const_iterator			end( void ) const { return std::stack<T>::c.end(); };
+		reverse_iterator		rbegin( void ) { return std::stack<T>::c.rbegin(); };
+		const_reverse_iterator	rbegin( void ) const { return std::stack<T>::c.rbegin(); };
+		reverse_iterator		rend( void ) { return std::stack<T>::c.rend(); };
+		const_reverse_iterator	rend( void ) const { return std::stack<T>::c.rend(); };
 };
 
 #endif
